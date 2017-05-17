dfcf41b5ede56ff757538da717c82d64f4ea25b9
John Schultz <jschultz@salesforce.com>
2017-05-17 13:20:17 -0600

    Adding server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  };
+};
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 3000);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
