a3fc146a86863c72d975a0e5cf57f468badc251f
John Schultz <jschultz@salesforce.com>
2017-05-18 22:30:22 -0600

    fixing url for api calls

--- src/app/fan-control/fan-control.service.ts
+++ src/app/fan-control/fan-control.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import * as request from 'request';
 
+let _window: any = window;
+
 @Injectable()
 export class FanControlService {
 
   constructor() { }
   
   callAction(control: string, action: string, token: string) {
-    let url: string = '/api/' + control;
+    let url: string = _window.location.protocol + '//' + _window.location.hostname + '/api/' + control;
     
     if (control === 'fan') {
       url += '/' + action;
