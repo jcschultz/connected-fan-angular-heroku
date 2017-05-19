641b67afeae8acbd76ddae1385425ddbe6e9e416
John Schultz <jschultz@salesforce.com>
2017-05-19 11:08:58 -0600

    adding feedback and adjusting styles

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -10,6 +10,9 @@ import { FanControlService } from '../fan-control/fan-control.service';
 })
 export class HomeComponentComponent implements OnInit {
   token: string;
+  activeButton: string;
+  actionPending = false;
+  errorMsg: string;
 
   constructor(private authService: AuthService, private router: Router, private fanControlService: FanControlService) { }
 
@@ -23,13 +26,35 @@ export class HomeComponentComponent implements OnInit {
   }
   
   onBtnClick(control: string, speed: string) {
+    this.activateButton(control, speed);
+    
     this.fanControlService.callAction(control, speed, this.token)
       .then((res) => {
+        this.resetButtons();
         console.log('successfully called fan action');
       })
       .catch((err) => {
+        this.resetButtons();
         console.error('error calling fan action', err);
+        this.errorMsg = 'Oops! Something went wrong. Try again.'
       });
+  }
+  
+  activateButton(control: string, speed: string) {
+    this.actionPending = true;
+    this.errorMsg = null;
+    
+    if (control === 'light') {
+      this.activeButton = 'light';
+    }
+    else {
+      this.activeButton = speed;
+    }
+  }
+  
+  resetButtons() {
+    this.actionPending = false;
+    this.activeButton = null;
   }
 
 }
